2491fe3f96389a6780adbdb98380b76f021410da
mobilesite <mobilesite@163.com>
2016-10-01 15:46:19 +0800

    update

--- js/ditto.js
+++ js/ditto.js
@@ -339,8 +339,7 @@ function router() {
             ds.async = true;
             ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
             ds.charset = 'UTF-8';
-            (document.getElementsByTagName('head')[0]
-            || document.getElementsByTagName('body')[0]).appendChild(ds);
+            (document.getElementsByTagName('body')[0]).appendChild(ds);
         })();
 
         // 加载disqus
