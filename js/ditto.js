eb77070e637091f287a6365fe4ee92a004b699f2
mobilesite <mobilesite@163.com>
2016-10-15 15:31:46 +0800

    update

--- js/ditto.js
+++ js/ditto.js
@@ -426,25 +426,25 @@ function router() {
             $('#pagedown').css('display', 'inline-block');
         }
 
-        // (function () {
-        //     var $w = $(window);
-        //     var $prog2 = $('.progress-indicator-2');
-        //     var wh = $w.height();
-        //     var h = $('body').height();
-        //     var sHeight = h - wh;
-        //     $w.on('scroll', function () {
-        //         window.requestAnimationFrame(function () {
-        //             var perc = Math.max(0, Math.min(1, $w.scrollTop() / sHeight));
-        //             updateProgress(perc);
-        //         });
-        //     });
-        //
-        //     function updateProgress(perc) {
-        //         $prog2.css({width: perc * 100 + '%'});
-        //         ditto.save_progress && store.set('page-progress', perc);
-        //     }
-        //
-        // }());
+        (function () {
+            var $w = $(window);
+            var $prog2 = $('.progress-indicator-2');
+            var wh = $w.height();
+            var h = $('body').height();
+            var sHeight = h - wh;
+            $w.on('scroll', function () {
+                window.requestAnimationFrame(function () {
+                    var perc = Math.max(0, Math.min(1, $w.scrollTop() / sHeight));
+                    updateProgress(perc);
+                });
+            });
+
+            function updateProgress(perc) {
+                $prog2.css({width: perc * 100 + '%'});
+                ditto.save_progress && store.set('page-progress', perc);
+            }
+
+        }());
 
         var countJs = document.createElement('script');
         countJs.src = 'https://dn-lbstatics.qbox.me/busuanzi/2.3/busuanzi.pure.mini.js';
